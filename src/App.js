28ddd6c496d79232ceae59813c1df9bb19b995eb
rwilt <rosie.wilt@gmail.com>
2020-08-13 15:57:50 -0400

    fonts

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey! I'm Rosie (flower). I'm a software engineer living in Brooklyn, NY (bodega flower) by way of New Jersey (white mana burger or diner). 
-        Prior to tech, I was in the luxury fashion space working in wholesale and merchandising for brands like RE/DONE (jeans). 
-        I'm actively seeking new roles, and I'm passionate about creativity (do something cool with the text), helping others, and learning new things.</h1>
+    <div className="intro">
+      <h1>Hey! I'm Rosie. I'm a software engineer living in Brooklyn by way of New Jersey.</h1> 
+ 
+
+       <h1> Prior to tech, I was in the luxury fashion space working in wholesale and merchandising for brands like RE/DONE.</h1>
+
+       <h1>I'm passionate about creativity, helping others, and learning new things, and actively seeking a new role.</h1>
+
+        <h1>Outside of that, I enjoy ___, ____, and ____. If you want to know more about my work - check out my <span className="highlight">projects</span>, and <span className="highlight">CV</span>.
+        Curious to chat more? <span className="highlight">Contact me</span>! </h1>
+
     </div>
   );
 }
