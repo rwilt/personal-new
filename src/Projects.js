e45c79444bfb67e08b08ac1f26bd9120de2c49fc
rwilt <rosie.wilt@gmail.com>
2020-08-17 19:53:14 -0400

    projects menu

--- src/Projects.js
+++ src/Projects.js
@@ -4,17 +4,27 @@ import Nav from './Nav';
 import {NavLink, Link} from 'react-router-dom';
 
 const Projects = (props) => {
+    let [clicked, setClick] = useState(false)
+
+    let handleClick = (e)=> {
+        setClick((prevState) => {return !prevState})
+    }
 
 return(
-    <div className="project-container"><button>X</button>
+
+    <div className="project-container"><a href ="/" className="close-btn">X</a>
 
     <div className="project-list">
-    <a href="/"><h1>Coachable</h1></a>
+    <a href="#"><h1>Coachable</h1></a>
     <a href="/"><h1>Change Agents</h1></a>
     <a href="/"><h1>The Farmers Market</h1></a>
     <a href="/"><h1>The Net Museum</h1></a>
 
     <a href="/"><h1>Writing</h1></a>
+    </div>
+
+    <div className="project-show">
+        
     </div>
     </div>
 )
