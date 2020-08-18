bf8cff28f8b0d10ec3cb1e956fc987db8802d668
rwilt <rosie.wilt@gmail.com>
2020-08-18 14:04:17 -0400

    contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,6 +11,7 @@ return(
         <div className="contact-list">
         <a href="https://www.linkedin.com/in/rosie-wilt/"><h1>LinkedIn</h1></a>
         <a href="https://github.com/rwilt"><h1>GitHub</h1></a> 
+        <a href="https://medium.com/@rosiewilt"><h1>Medium</h1></a>
         <a href={`mailto:${"rosie.wilt@gmail.com"}?subject=Hi%20Rosie%20:)!&body=Hey Rosie!%0D%0A%0D%0A I wanted to reach out about...[your message here - ideas: job opportunities, my projects, skincare, favorite breed of dog, Buffy and Spike vs. Buffy and Angel, etc.].`}><h1>E-mail</h1></a>
         </div>
     </div>
